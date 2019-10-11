9750f96e0eacb04cb7d6e776ccb425385564361c
briceyokoyama <briceyokoyama@gmail.com>
2019-10-10 22:42:19 -0700

    simple pure node.js server

--- purenodeserver.js
+++ purenodeserver.js
@@ -4,13 +4,38 @@ const url = require('url');
 function handler(req, res) {
   const parsedUrl = url.parse(req.url, true);
 
+  res.setHeader('x-server-date', new Date());
+
   if (parsedUrl.pathname === '/') {
     res.writeHead(200, {'Content-type':'text/plain'});
     res.write('Hello, I am a webserver!');
-    res.end();
+    return res.end();
+  } else if(parsedUrl.pathname === '/time') {
+    res.writeHead(200, {'Content-type':'text/plain'});
+    res.write(new Date().toString());
+    return res.end();
+  } else if(parsedUrl.pathname === '/hello') {
+    const name = parsedUrl.query.name;
+    if(!name) {
+      res.writeHead(400, {'Content-type':'text/plain'});
+      return res.end();
+    }
+    res.writeHead(200, {'Content-type':'text/plain'});
+    res.write(`Hello ${parsedUrl.query.name}`);
+    return res.end();
+  } else if(parsedUrl.pathname.startsWith('/user/')) {
+    const regex = new RegExp('\/user\/(.+)');
+    const matches = regex.exec(parsedUrl.pathname);
+    if(!matches || !matches[1]) {
+      res.writeHead(400, {'Content-type':'text/plain'});
+      return res.end();
+    }
+    res.writeHead(200, {'Content-type':'text/plain'});
+    res.write(`Userprofile of ${matches[1]}`);
+    return res.end();
   } else {
     res.writeHead(404, {'Content-type':'text/plain'});
-    res.end()
+    return res.end();
   }
 }
 
