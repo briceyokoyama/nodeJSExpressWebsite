83e0fdbc336e5d406734e128c691f3e934c19a60
briceyokoyama <briceyokoyama@gmail.com>
2019-10-12 01:23:52 -0700

    add pug index and error template

--- work/conference/server/index.js
+++ work/conference/server/index.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const createError = require('http-errors');
+const path = require('path');
 const app = express();
+
+app.set('view engine', 'pug');
+if(app.get('env') === 'development') {
+  app.locals.pretty = true;
+}
+app.set('views', path.join(__dirname, './views'));
 
 const routes = require('./routes');
 app.use(express.static('public'));
@@ -7,6 +15,19 @@ app.get('/favicon.ico', (reg, res, next) => {
   return res.sendStatus(204);
 })
 app.use('/', routes());
+
+app.use((req, res, next) => {
+  return next(createError(404, 'File not found'));
+})
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  const status = err.status || 500;
+  res.locals.status = status;
+  res.locals.error = req.app.get('env') === 'development' ? err: {};
+  res.status(status);
+  return res.render('error');
+})
 
 app.listen(3000);
 module.export = app;
